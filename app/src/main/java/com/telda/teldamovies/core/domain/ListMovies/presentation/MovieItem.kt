6fdc60a78185eb5ad6b266c93aa78e791315b7d4
Mina Ayad <mina.ayad2020@gmail.com>
2025-06-01 22:06:40 +0300

    - Add placeholder to if the image of movie is not exist. - Enhance the design of List movies.

--- app/src/main/java/com/telda/teldamovies/core/domain/ListMovies/presentation/MovieItem.kt
+++ app/src/main/java/com/telda/teldamovies/core/domain/ListMovies/presentation/MovieItem.kt
@@ -2,45 +2,141 @@ package com.telda.teldamovies.core.domain.ListMovies.presentation
 
 import android.util.Log
 import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.*
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+import com.telda.teldamovies.R
 import com.telda.teldamovies.core.data.model.Movie
-import com.telda.teldamovies.core.di.NetworkModule
+
 
 @Composable
-fun MovieItem(movie: Movie, isInWatchlist: Boolean, onClick: () -> Unit) {
+fun MovieGridItem(
+    movie: Movie,
+    onClick: () -> Unit
+) {
     Card(
         modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth()
-            .clickable { onClick() },
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+            .wrapContentSize()
+            .padding(10.dp)
+            .clickable(onClick = onClick),
+        elevation = CardDefaults.cardElevation(8.dp)
     ) {
-        Row(Modifier.padding(8.dp)) {
-            val imageUrl = "https://image.tmdb.org/t/p/w500${movie.poster_path}"
-            Log.e("imageUrl", imageUrl)
-            AsyncImage(
-                model = imageUrl,
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            SubcomposeAsyncImage(
+                model = "https://image.tmdb.org/t/p/w500${movie.poster_path}",
                 contentDescription = movie.title,
-                modifier = Modifier.size(100.dp)
-            )
-            Spacer(Modifier.width(8.dp))
-            Column {
-                Text(text = movie.title, style = MaterialTheme.typography.titleMedium)
-                Text(text = movie.overview.take(100) + "...", style = MaterialTheme.typography.bodyMedium)
-                if (isInWatchlist) {
-                    Text("★ In Watchlist", color = androidx.compose.ui.graphics.Color.Yellow)
+                modifier = Modifier
+                    .size(200.dp,250.dp)
+                    .clip(RoundedCornerShape(10.dp)),
+                contentScale = ContentScale.Crop
+            ) {
+                when (painter.state) {
+                    is AsyncImagePainter.State.Loading -> {
+                        Box(
+                            modifier = Modifier
+                                .size(200.dp,250.dp)
+                                .background(androidx.compose.ui.graphics.Color.White.copy(alpha = 0.1f)),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.size(24.dp),
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                        }
+                    }
+                    is AsyncImagePainter.State.Error -> {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(androidx.compose.ui.graphics.Color.White.copy(alpha = 0.1f)),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            androidx.compose.foundation.Image(
+                                painter = painterResource(id = R.drawable.ic_placeholder),
+                                contentDescription = "Placeholder image",
+                                modifier = Modifier
+                                    .size(200.dp,250.dp),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                    }
+                    else -> {
+                        SubcomposeAsyncImageContent()
+                    }
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .background(androidx.compose.ui.graphics.Color.LightGray.copy(alpha = 0.7f))
+                    .padding(6.dp)
+            ) {
+                Text(
+                    text = movie.title,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    color = androidx.compose.ui.graphics.Color.Black,
+                    fontWeight = FontWeight.ExtraBold,
+                    maxLines = 1,
+                    style = TextStyle(
+                        shadow = Shadow(
+                            Color(0xFFFFFFFF),
+                            offset = Offset(1f, 1f),
+                            5f
+                        )
+                    )
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    modifier = Modifier.align(Alignment.End)
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Star,
+                        contentDescription = "",
+                        tint = androidx.compose.ui.graphics.Color.Yellow
+                    )
+                    Text(
+                        text = movie.vote_average.toString(),
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 8.dp),
+                        fontWeight = FontWeight.Bold,
+                        color = androidx.compose.ui.graphics.Color.Black,
+                        maxLines = 2
+                    )
                 }
             }
         }
--- app/src/main/java/com/telda/teldamovies/core/domain/ListMovies/presentation/MoviesListScreen.kt
+++ app/src/main/java/com/telda/teldamovies/core/domain/ListMovies/presentation/MoviesListScreen.kt
@@ -1,34 +1,41 @@
 package com.telda.teldamovies.core.domain.ListMovies.presentation
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.telda.teldamovies.core.data.model.Movie
-
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+import com.telda.teldamovies.R
+import com.telda.teldamovies.core.data.model.Movie
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun MoviesListScreen(
     viewModel: MoviesListViewModel = hiltViewModel(),
@@ -37,44 +44,71 @@ fun MoviesListScreen(
     var searchQuery by rememberSaveable { mutableStateOf("") }
     val movies = viewModel.movies
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        // Search bar at the top
-        TextField(
-            value = searchQuery,
-            onValueChange = { query ->
-                searchQuery = query
-                viewModel.searchMovies(query)
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            placeholder = { Text("Search movies...") },
-            singleLine = true,
-            leadingIcon = { Icon(Icons.Default.Search, contentDescription = "Search Icon") },
-            colors = TextFieldDefaults.textFieldColors(containerColor = MaterialTheme.colorScheme.surface)
-        )
-
-        if (searchQuery.isNotEmpty() && movies.isEmpty()) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = "No match found",
-                    style = MaterialTheme.typography.titleMedium
+    Scaffold(
+        modifier = Modifier.background(Color.Transparent),
+        topBar = {
+            TopAppBar(
+                title = { Text("Telda Movies", color = Color.White) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Black.copy(alpha = 1f)
                 )
-            }
-        } else {
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                itemsIndexed(movies) { index, movie ->
-                    MovieItem(movie = movie, isInWatchlist = false) {
-                        onMovieClick(movie)
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            // Search bar
+            TextField(
+                value = searchQuery,
+                onValueChange = { query ->
+                    searchQuery = query
+                    if (query.isEmpty()) {
+                        viewModel.resetPagination()
+                        viewModel.loadPopularMovies()
+                    } else {
+                        viewModel.searchMovies(query)
                     }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 20.dp, end = 20.dp, top = 8.dp),
+                placeholder = { Text("Search movies...") },
+                singleLine = true,
+                leadingIcon = { Icon(Icons.Default.Search, contentDescription = "Search Icon") },
+                colors = TextFieldDefaults.textFieldColors(containerColor = MaterialTheme.colorScheme.surface)
+            )
 
-                    // Trigger pagination when the user scrolls near the end
-                    if (index >= movies.size - 5 && searchQuery.isEmpty()) {
-                        LaunchedEffect(Unit) {
-                            viewModel.loadPopularMovies()
+            if (searchQuery.isNotEmpty() && movies.isEmpty()) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "No match found",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
+            } else {
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Transparent)
+                        .padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                ) {
+                    itemsIndexed(movies) { index, movie ->
+                        MovieGridItem(
+                            movie = movie,
+                            onClick = { onMovieClick(movie) }
+                        )
+
+                        if (index >= movies.size - 5 && searchQuery.isEmpty()) {
+                            LaunchedEffect(Unit) {
+                                viewModel.loadPopularMovies()
+                            }
                         }
                     }
                 }
